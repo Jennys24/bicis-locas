84d729de68d871afa7fdde3afc5aa1c681570ab0
Jennifer Fermin <jennifer.ferminp@gmail.com>
2020-12-21 10:36:05 -0300

    se experimenta en htmlm la validación de texto con required

--- js/main.js
+++ js/main.js
@@ -9,9 +9,9 @@ function validarEmail(valor){
 function validateForm() {
 	$('.prevent').remove();
 	var fnombre = $('#name').val().trim();
-	if (fnombre == '') {
+	/* if (fnombre == '') {
 		$('#name').parent().append('<span class="prevent">Debes registrar un nombre</span>');
-	} else if( /^[a-zA-Z ]+$/.test(fnombre) == false){
+	} else */ if( /^[a-zA-Z ]+$/.test(fnombre) == false){
 		$('#name').parent().append('<span class="prevent">Ingresa sólo letras.</span>'); 
 	} else if(/^[A-Z]+$/.test(fnombre[0]) == false){
 		$('#name').parent().append('<span class="prevent">La primera letra debe ser mayúsculas.</span>'); 
